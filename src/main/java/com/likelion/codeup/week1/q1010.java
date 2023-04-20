f8bd7ecdb12e4584ac8e485194647e3972df5954
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-20 13:44:21 +0900

    Solved 1010~1013 in codeup

---
+++ src/main/java/com/likelion/codeup/week1/q1010.java
@@ -0,0 +1,11 @@
+package com.likelion.codeup.week1;
+
+import java.util.Scanner;
+
+public class q1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%s",n);
+    }
+}
---
+++ src/main/java/com/likelion/codeup/week1/q1012.java
@@ -0,0 +1,10 @@
+package com.likelion.codeup.week1;
+
+import java.util.Scanner;
+
+public class q1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%f", sc.nextDouble());
+    }
+}
