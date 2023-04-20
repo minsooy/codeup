8a01ed15395120acdd18492c8108475718d5a799
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-20 15:38:25 +0900

    solved 1014 in codeup

---
+++ src/main/java/com/likelion/codeup/week1/q1014.java
@@ -0,0 +1,14 @@
+package com.likelion.codeup.week1;
+
+import java.util.Scanner;
+
+public class q1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String val1= sc.next();
+        String val2= sc.next();
+        System.out.printf("%s %s", val2,val1);
+
+
+    }
+}
