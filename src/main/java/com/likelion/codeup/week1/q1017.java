cab77683df45958f88dc41e1eddd8e37687c1fbc
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-21 10:41:06 +0900

    Solved 1017in codeup

---
+++ src/main/java/com/likelion/codeup/week1/q1017.java
@@ -0,0 +1,11 @@
+package com.likelion.codeup.week1;
+
+import java.util.Scanner;
+
+public class q1017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.printf("%d %d %d", i,i,i);
+    }
+}
