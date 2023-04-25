f6a7cfdd622485e65a6bcd41b8317ddc0113281b
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-25 09:16:52 +0900

    Sovled 1031~1037 in codeup

---
+++ src/main/java/com/likelion/codeup/week2/q1033.java
@@ -0,0 +1,11 @@
+package com.likelion.codeup.week2;
+
+import java.util.Scanner;
+
+public class q1033 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.printf("%X",sc.nextInt());
+        }
+}
+
---
+++ src/main/java/com/likelion/codeup/week2/q1036.java
@@ -0,0 +1,10 @@
+package com.likelion.codeup.week2;
+
+import java.util.Scanner;
+
+public class q1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)sc.next().charAt(0));
+    }
+}
