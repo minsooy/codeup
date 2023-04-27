2f7a044e976cf4853d229df5cb57d66a1ec644b4
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-27 11:21:01 +0900

    Solved 1049~1052 in codeup

---
+++ src/main/java/com/likelion/codeup/week2/day4/q1051.java
@@ -0,0 +1,13 @@
+package com.likelion.codeup.week2.day4;
+
+import java.util.Scanner;
+
+public class q1051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int var1 = sc.nextInt();
+        int var2 = sc.nextInt();
+
+        System.out.printf("%d", var1 <= var2 ? 1 : 0);
+    }
+}
