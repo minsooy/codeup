655dc03eef632b5ad1c344281431dd49d3546a08
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-27 14:27:11 +0900

    Solved 1047~1048 in codeup

---
+++ src/main/java/com/likelion/codeup/week2/day4/q1047.java
@@ -0,0 +1,10 @@
+package com.likelion.codeup.week2.day4;
+
+import java.util.Scanner;
+
+public class q1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() <<1);
+    }
+}
---
+++ src/main/java/com/likelion/codeup/week2/day4/q1048.java
@@ -0,0 +1,13 @@
+package com.likelion.codeup.week2.day4;
+
+import java.util.Scanner;
+
+public class q1048 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        System.out.println((long)val1 << val2);
+    }
+}
