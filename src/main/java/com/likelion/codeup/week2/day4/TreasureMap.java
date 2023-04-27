93f30d62606573281b4052bdcfe3f5b0ca8182ee
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-27 15:31:06 +0900

    Solved 1063~1064 in codeup and solved Programmers TreasureMap

---
+++ src/main/java/com/likelion/codeup/week2/day4/TreasureMap.java
@@ -0,0 +1,17 @@
+package com.likelion.codeup.week2.day4;
+
+public class TreasureMap {
+    class Solution {
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] map = new String[n];
+
+            for(int i = 0; i < n;  i++){
+                map[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]));
+                map[i].replaceAll("1","#");
+                map[i].replaceAll("0"," ");
+            }
+            return map;
+        }
+    }
+
+}
---
+++ src/main/java/com/likelion/codeup/week2/day4/q1064.java
@@ -0,0 +1,15 @@
+package com.likelion.codeup.week2.day4;
+
+import java.util.Scanner;
+
+public class q1064 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        int val3 = sc.nextInt();
+
+        System.out.println((val1 < val2 ? val1 : val2) < val3 ? (val1 < val2 ? val1 : val2) : val3);
+
+    }
+}
