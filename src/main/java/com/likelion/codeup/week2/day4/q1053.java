c44c89aa513c5fd872be50fe892cc0863937ecb9
Minsoo Yeo <msyeo1404@gmail.com>
2023-04-27 13:20:04 +0900

    Solved 1053~1055 in codeup

---
+++ src/main/java/com/likelion/codeup/week2/day4/q1053.java
@@ -0,0 +1,10 @@
+package com.likelion.codeup.week2.day4;
+
+import java.util.Scanner;
+
+public class q1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println( sc.nextInt() == 0 ? 1 : 0);
+    }
+}
