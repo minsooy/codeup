d58e0f1a1a3652a0a122952daea120e4b51c1989
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 11:41:29 +0900

    Solved 1073~1075 in codeup

---
+++ src/main/java/com/likelion/codeup/week3/q1073.java
@@ -0,0 +1,16 @@
+package com.likelion.codeup.week3;
+
+import java.util.Scanner;
+
+public class q1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("0")){
+                break;
+            }
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/main/java/com/likelion/codeup/week3/q1074.java
@@ -0,0 +1,14 @@
+package com.likelion.codeup.week3;
+
+import java.util.Scanner;
+
+public class q1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int index = sc.nextInt();
+
+        for (int i = index; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
