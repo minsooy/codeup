99882ec21aa70806c5871755e5e5f9983207a298
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 13:47:02 +0900

    Solved 1076~1079 in codeup

---
+++ src/main/java/com/likelion/codeup/week3/NumberSwitchQuery.java
@@ -0,0 +1,15 @@
+package com.likelion.codeup.week3;
+
+public class NumberSwitchQuery {
+
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            int temp = 0;
+            temp = arr[queries[i][0]];
+            arr[queries[i][0]] =  arr[queries[i][1]];
+            arr[queries[i][1]] =  temp;
+
+        }
+        return arr;
+    }
+}
