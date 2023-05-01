51424d90ab86d1126ea0c11761b98e9acfdbaaac
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 10:52:24 +0900

    Solved 1071~1072 in codeup and DiceGameTwo in Programmers

---
+++ src/main/java/com/likelion/codeup/week3/DiceGameTwo.java
@@ -0,0 +1,23 @@
+package com.likelion.codeup.week3;
+
+public class DiceGameTwo {
+    class Solution {
+        int answer = 0;
+
+        public int solution(int a, int b, int c) {
+            if(a != b && b != c && c != a){
+                answer = a + b + c;
+            }
+
+            if((a == b && a != c)|| (b == c && b != a) || (c == a && c != b) ){
+                answer = (int) ((int) (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2)  + Math.pow(c, 2)));
+            }
+
+            if(a == b && b == c && c == a){
+                answer = (int) (27 * Math.pow(a , 6));
+            }
+
+            return answer;
+        }
+    }
+}
