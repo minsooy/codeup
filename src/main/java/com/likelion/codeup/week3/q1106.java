1854155249fd7f702123bf615940f5ebe174dd4e
Minsoo Yeo <msyeo1404@gmail.com>
2023-05-01 09:08:24 +0900

    Solved q1103 q 1106 in codeup

---
+++ src/main/java/com/likelion/codeup/week3/q1106.java
@@ -0,0 +1,8 @@
+package com.likelion.codeup.week3;
+
+public class q1106 {
+    public static void main(String[] args) {
+        System.out.println(0x80000000);
+        System.out.println(0x7fffffff);
+    }
+}
